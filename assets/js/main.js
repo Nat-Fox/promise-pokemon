c9d6d8b7f08d635108d989cb069eb43d89d358a9
Natalia Garrido <natgramirez@outlook.com>
2017-07-12 23:28:35 -0400

    obteniendo data faltante

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,44 +3,64 @@ function firstMayus(str) {
 }
 
 $.ajax({
-    url: 'http://pokeapi.co/api/v2/pokemon',
-    type: 'GET',
-    datatype: 'JSON',
-    data: { 'limit': '12' },
-})
-
-.done(function(respuesta) {
-        respuesta.results.forEach(function(data) {
-            console.log('data', data);
+        url: 'http://pokeapi.co/api/v2/pokemon',
+        type: 'GET',
+        datatype: 'JSON',
+        data: { 'limit': '12' },
+    })
+    // Ocurre despues de llamar al ajax
+    .done(function(respuesta) {
+        respuesta.results.forEach(function(data, indice) {
+            //console.log('data', data);
             $('#nombre-pokemones').append(
                 `<div class="row center">
                     <div class="col s3"><span class="card-content">${firstMayus(data.name)}</span></div>
-                    <div class="col s3"><span class="card-content">2.</span></div>
-                    <div class="col s3"><span class="card-content">2.</span></div>
-                    <div class="col s3"><span class="card-content">2.</span></div>
+                    <div class="col s3"><span id="first-ability-${indice}" class="card-content"></span></div>
+                    <div class="col s3"><span id="total-abilities-${indice}" class="card-content"></span></div>
+                    <div class="col s3"><span id="types-${indice}" class="card-content"></span></div>
                 </div>`
             );
 
         })
     })
-    .done(function() {
-
-        console.log('Este es el mensaje de después de llama ajax');
-    })
     //Llamo a la segunda api
     .done(function(secondApi) {
-        $.ajax({
-                url: 'http://pokeapi.co/api/v2/pokemon/1/',
-                type: 'GET',
-                datatype: 'JSON',
-                data: { 'limit': '15' },
-            })
-            .done(function(data) {
-                Array.from(data.abilities).forEach(function(e) {
-                    console.log(e.ability.name)
+        secondApi.results.forEach(function(data, indice) {
+            console.log(data);
+            // llamada a la nueva api
+            $.ajax({
+                    url: data.url,
+                    type: 'GET',
+                    datatype: 'JSON',
+                    data: { 'limit': '1' },
                 })
+                .done(function(respuesta) {
+                    console.log('respuesta', respuesta.types)
+                        // primera habilidad
+                    $(`#first-ability-${indice}`).append(respuesta.abilities[0].ability.name);
+                    // todas las habilidades
+                    respuesta.abilities.forEach(function(ability) {
+                        console.log('ability', ability.ability.name);
+                        $(`#total-abilities-${indice}`).append(`<span>${ability.ability.name}<span><br>`);
+                    });
 
-            })
+                    respuesta.types.forEach(function(type) {
+                        console.log('type', type);
+                        $(`#types-${indice}`).append(`<span>${type.type.name}<span><br>`);
+                    })
+
+                }).fail(function() {
+                    console.log('error en la segunda llamada de la api')
+                })
+        });
+
+        //     .done(function(data) {
+        //         Array.from(data.abilities).forEach(function(e) {
+        //             console.log(e.ability.name)
+        //         })
+
+        //     })
+
     })
 
 
